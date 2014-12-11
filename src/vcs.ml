efd74197ac0c7f7a5797026179aaa76f0ffa6fca
lrraymond13 <lrraymond13@gmail.com>
2014-12-11 16:55:24 -0500

    copying - WIP

--- src/vcs.ml
+++ src/vcs.ml
@@ -1,16 +1,24 @@
 open Core.Std
 
-let copy path = 
-	print_string path;
-	print_string "\n"
 
+let copy_file to_path from_path = 
+	(*In_channel.with_file path ~f:*)
+	()
 
+let copy_dir to_path from_path = 
+	()
+
+let copy to_path from_path = 
+	if Sys.is_directory_exn from_path
+	then copy_dir to_path from_path
+	else copy_file to_path from_path
 
 let main () = 
-	let dir_list = Sys.ls_dir (Sys.getcwd ()) in 
-	List.iter ~f:copy dir_list 
+	let cwd = Sys.getcwd () in 
+	let copy_to = cwd ^ ".bak" in 
+	let dir_list = Sys.ls_dir cwd in 
+	List.iter ~f:(copy copy_to) dir_list 
 	
-
 
 
 let () = 
