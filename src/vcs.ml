64b642a2e307660ddc352ac79e54bc83f86cf8a3
lrraymond13 <lrraymond13@gmail.com>
2014-12-11 17:20:25 -0500

    copied files; on to dir

--- src/vcs.ml
+++ src/vcs.ml
@@ -1,9 +1,15 @@
 open Core.Std
 
-
 let copy_file to_path from_path = 
-	(*In_channel.with_file path ~f:*)
-	()
+
+	let to_file_path = to_path^"/"^from_path in 
+	let in_ch = In_channel.create from_path in 
+	let out_ch = Out_channel.create to_file_path in 
+	Out_channel.output_string out_ch (In_channel.input_all in_ch) ;
+	Out_channel.flush out_ch;
+	Out_channel.close out_ch;
+	In_channel.close in_ch
+
 
 let copy_dir to_path from_path = 
 	()
@@ -15,7 +21,8 @@ let copy to_path from_path =
 
 let main () = 
 	let cwd = Sys.getcwd () in 
-	let copy_to = cwd ^ ".bak" in 
+	let copy_to = cwd ^ ".bak" in
+	Unix.mkdir copy_to;
 	let dir_list = Sys.ls_dir cwd in 
 	List.iter ~f:(copy copy_to) dir_list 
 	
