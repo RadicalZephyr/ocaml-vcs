780de74b9a93e0a8bbd855709f3eb3869e5e8d56
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-12 21:12:10 -0500

    added snapshotting capabilities

--- src/vcs.ml
+++ src/vcs.ml
@@ -11,6 +11,7 @@ let copy_file from_path to_path =
   let out_ch = Out_channel.create to_path in
   let len = 4096 in
   let buf = String.create len in
+
   let rec copy () =
     match In_channel.input in_ch ~buf ~pos:0 ~len with
     | 0 -> ()
@@ -23,6 +24,7 @@ let copy_file from_path to_path =
   In_channel.close in_ch;
   Out_channel.close out_ch
 
+
 let rec copy from_root to_root item =
   let from_path = Filename.concat from_root item in
   let   to_path = Filename.concat   to_root item in
@@ -34,14 +36,35 @@ let rec copy from_root to_root item =
      Sys.ls_dir from_path
      |> List.iter ~f:(fun item ->
                       copy from_path to_path item)
+let match_last inputList =
+  match List.last inputList with
+  | None -> 0
+  | Some x -> x
+
+
+
+let backup_name root =
+  let backup_dir = Filename.concat root ".myvcs" in
+  ensure_dir_exists backup_dir;
+  Sys.ls_dir backup_dir
+  |>  List.map ~f:Int.of_string
+  |> List.sort ~cmp:Int.compare
+  |> match_last
+  |> Int.succ
+  |> Int.to_string
+  |> Filename.concat backup_dir
+
+
+
 
 
 let main () =
   let cwd = Sys.getcwd () in
   let root = Filename.dirname cwd in
-  let dst = Filename.concat root ".myvcs" in
-  ensure_dir_exists dst;
-  copy cwd dst "."
+  let new_backup = backup_name root in
+  ensure_dir_exists new_backup;
+  print_string new_backup;
+  copy cwd new_backup "."
 
 
 let () =
