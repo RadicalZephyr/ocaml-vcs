e61d70449812edc8e73e666ef95cb6877d32124f
lrraymond13 <lrraymond13@gmail.com>
2014-12-13 14:57:26 -0500

    added checkout revision'

--- src/vcs.ml
+++ src/vcs.ml
@@ -59,9 +59,22 @@ let backup () =
   ensure_dir_exists new_backup;
   copy cwd new_backup "."
 
-let checkout revnum =
-  printf "Checkout revision number: %d\n" revnum
+let find_backup_folder root revnum = 
+  let backup_dir = Filename.concat root ".myvcs" in 
+  match Sys.file_exists backup_dir with 
+  | `No | `Unknown -> invalid_arg "Root Backup folder .myvcs does not exist"
+  | `Yes -> 
+      let backup_version = Filename.concat backup_dir (Int.to_string revnum) in 
+      match Sys.file_exists backup_version with
+      | `No | `Unknown -> invalid_arg (sprintf "Backup Version %d does not exist" revnum)
+      | `Yes -> backup_version
 
+let checkout revnum =
+  let cwd = Sys.getcwd () in 
+  let root = Filename.dirname cwd in
+  let backup_version = find_backup_folder root revnum in 
+  printf "Checking out Revision %d\n" revnum;
+  copy backup_version cwd "."
 
 let backup_command =
   Command.basic ~summary:""
