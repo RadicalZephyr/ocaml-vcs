74928a4d8396693083562325dacaf4877fa958d8
Geoff Shannon <geoffpshannon@gmail.com>
2014-12-12 19:13:51 -0500

    Inline copy_dir into copy function

--- src/vcs.ml
+++ src/vcs.ml
@@ -23,19 +23,17 @@ let copy_file from_path to_path =
   In_channel.close in_ch;
   Out_channel.close out_ch
 
-let rec copy_dir from_path to_path =
-  ensure_dir_exists to_path;
-  Sys.ls_dir from_path
-  |> List.iter ~f:(fun item ->
-                   copy from_path to_path item)
-
-and copy from_root to_root item =
+let rec copy from_root to_root item =
   let from_path = Filename.concat from_root item in
   let   to_path = Filename.concat   to_root item in
   match Sys.is_directory from_path with
   | `Unknown -> ()
   | `No -> copy_file from_path to_path
-  | `Yes -> copy_dir from_path to_path
+  | `Yes ->
+     ensure_dir_exists to_path;
+     Sys.ls_dir from_path
+     |> List.iter ~f:(fun item ->
+                      copy from_path to_path item)
 
 
 let main () =
